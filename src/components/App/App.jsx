15b0bdf99f6edc862ef76b8ff5ea13fa6ddf552b
Nastea <anastasia.iatco@isa.utm.md>
2021-10-28 15:37:30 +0300

    Add navbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,17 +10,23 @@ import {
 
 import { LinkButton } from '../../elements';
 
+import Navbar from '../Navbar/Navbar';
 import About from '../../routes/About';
 import Home from '../../routes/Home';
 import Topics from '../../routes/Topics';
+import Footer from '../Footer/Footer';
 
 const App = () => {
   return (
+    <div className="content wrap">
     <Router>
       <div>
-        <LinkButton to="/" type="link">home</LinkButton>
+      <Navbar /> 
+      </div>
+      <div>
+        {/* <LinkButton to="/" type="link">home</LinkButton>
         <LinkButton to="/about" type="link">About</LinkButton>
-        <LinkButton to="/topics" type="link">Topics</LinkButton>
+        <LinkButton to="/topics" type="link">Topics</LinkButton>*/} 
 
         <Switch>
           <Route path="/about">
@@ -35,6 +41,9 @@ const App = () => {
         </Switch>
       </div>
   </Router>
+
+  <Footer />
+  </div>
   );
 };
 
